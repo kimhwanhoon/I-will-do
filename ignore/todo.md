1066e32f9b2b6b4fb5bdc8f0d094d43475477f94
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-07-05 21:56:32 +0900

    Packages added
    1. react-router-dom
    2. redux
    3. react-redux
    4. react-query
    5. axios
    6. firebase

---
+++ ignore/todo.md
@@ -0,0 +1,7 @@
+installed packages:
+
+1. react-router-dom
+2. redux
+3. react-redux
+4. react-query
+5. axios
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from 'shared/Router';
+import { Provider } from 'react-redux';
+import store from 'redux/config/configStore';
+import './reset.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
